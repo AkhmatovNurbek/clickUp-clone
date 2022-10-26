6044b363c4494f3be74d97deb0b23305e890a2e0
Nurbek <@axmatov0713@gmail.com>
2022-10-26 18:42:34 -0400

    init

---
+++ src/main/java/uz/clickup/clickupclone/ClickUpCloneApplication.java
@@ -0,0 +1,13 @@
+package uz.clickup.clickupclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClickUpCloneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClickUpCloneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/clickup/clickupclone/ClickUpCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.clickup.clickupclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClickUpCloneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
